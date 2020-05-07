88571d37da653943c2834e2e56d85371c1726dff
Manh Thang Dao <manhthang177@gmail.com>
2020-05-07 16:54:02 +0700

    state demo

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, View} from 'react-native';
 
 function HelloWorldApp() {
+  const [text, setText] = useState('');
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40, borderColor: 'grey', borderWidth: 1}}
+        placeholder="Type here!"
+        onChangeText={text => setText(text)}
+        value={text}
+      />
     </View>
   );
 }
