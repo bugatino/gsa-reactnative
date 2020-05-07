dc61b3b30790c86d881bcee090b9d04166c5e503
Manh Thang Dao <manhthang177@gmail.com>
2020-05-07 17:15:23 +0700

    image component

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
 
 function HelloWorldApp() {
   return (
@@ -9,7 +9,17 @@ function HelloWorldApp() {
         justifyContent: 'center',
         alignItems: 'center',
       }}>
-      <Text>Hello, world!</Text>
+      <Image
+        source={{uri: 'https://reactnative.dev/docs/assets/p_cat2.png'}}
+        style={{width: 200, height: 200}}
+      />
+      <Text style={{marginBottom: 35}}>This is a cat!</Text>
+
+      <Image
+        source={require('./assets/image/logo_cx.jpg')}
+        style={{width: '100%', resizeMode: 'contain'}}
+      />
+      <Text>Drive your way</Text>
     </View>
   );
 }
