5916fb4c4290091a24c0b133a51b67fc51565eab
Manh Thang Dao <manhthang177@gmail.com>
2020-05-13 20:08:42 +0700

    alignItems demo

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ function HelloWorldApp() {
       <View style={styles.box1} />
       <View style={styles.box2} />
       <View style={styles.box3} />
+      <View style={styles.box4} />
     </View>
   );
 }
@@ -15,11 +16,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    alignItems: 'stretch',
   },
   box1: {width: 50, height: 50, backgroundColor: 'powderblue'},
-  box2: {width: 50, height: 50, backgroundColor: 'skyblue'},
-  box3: {width: 50, height: 50, backgroundColor: 'steelblue'},
+  box2: {width: 150, height: 50, backgroundColor: 'skyblue'},
+  box3: {width: 250, height: 100, backgroundColor: 'steelblue'},
+  box4: {height: 100, backgroundColor: 'orange'},
 });
 
 export default HelloWorldApp;
