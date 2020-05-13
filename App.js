f64a4a0b9676c2bde7459f27e14912606f352183
Manh Thang Dao <manhthang177@gmail.com>
2020-05-13 19:57:04 +0700

    justifyContent demo

--- App.js
+++ App.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
 function HelloWorldApp() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
+    <View style={styles.container}>
+      <View style={styles.box1} />
+      <View style={styles.box2} />
+      <View style={styles.box3} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  box1: {width: 50, height: 50, backgroundColor: 'powderblue'},
+  box2: {width: 50, height: 50, backgroundColor: 'skyblue'},
+  box3: {width: 50, height: 50, backgroundColor: 'steelblue'},
+});
+
 export default HelloWorldApp;
